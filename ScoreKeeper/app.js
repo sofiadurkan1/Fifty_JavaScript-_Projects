d11682bba59c3e6a00f53d7b1896e8af5ae17864
sofia durkan <sofiadurkan@gmail.com>
2021-07-28 23:53:46 -0500

    completed

--- ScoreKeeper/app.js
+++ ScoreKeeper/app.js
@@ -1,7 +1,9 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
 
 
 let p1Score = 0;
@@ -15,6 +17,9 @@ p1Button.addEventListener('click',function() {
         p1Score++;
     if(p1Score === winningScore){
         isGameOver = true;
+        p1Display.classList.add('winner');
+        p2Display.classList.add('loser');
+
 
     }
         p1Display.textContent = p1Score;
@@ -29,6 +34,8 @@ p2Button.addEventListener('click',function() {
         p2Score++;
     if(p2Score === winningScore){
         isGameOver = true;
+        p2Display.classList.add('winner');
+        p1Display.classList.add('loser');
 
     }
         p2Display.textContent = p2Score;
@@ -38,5 +45,24 @@ p2Button.addEventListener('click',function() {
 
 });
 
+winningScoreSelect.addEventListener("change",function() {
+    winningScore = parseInt(this.value);
+    reset();
+
+});
 
 
+
+resetButton.addEventListener('click',reset) 
+
+function reset(){
+    isGameOver=false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = p1Score;
+    p2Display.textContent = p2Score;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+
+}
+    
