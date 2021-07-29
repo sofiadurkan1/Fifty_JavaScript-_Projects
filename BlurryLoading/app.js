794b6e81b7d87212517f5e0593ec1e25d06157fb
sofia durkan <sofiadurkan@gmail.com>
2021-07-29 08:40:37 -0500

    completed

--- BlurryLoading/app.js
+++ BlurryLoading/app.js
@@ -1,2 +1,21 @@
 const loadingText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
+
+let load = 0;
+let int = setInterval(blurring,30)
+function blurring (){
+    load++;
+    if(load>99){
+        clearInterval(int);
+    }
+    loadingText.innerHTML = `${load}%`
+    loadingText.style.opacity =scale(load, 0, 100,1, 0);
+    bg.style.filter = `blur(${scale(load,0,100,20,0)}px)`
+
+
+
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
