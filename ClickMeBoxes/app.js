58be63975200d49ebb2f4148faa17814b48f4ef0
sofia durkan <sofiadurkan@gmail.com>
2021-07-30 07:31:39 -0500

    js added

---
+++ ClickMeBoxes/app.js
@@ -0,0 +1,28 @@
+const makeRandomColor = () => {
+    const r = Math.floor(Math.random() *255);
+    const g = Math.floor(Math.random() *255);
+    const b = Math.floor(Math.random() *255);
+    return `rgb(${r}, ${g}, ${b})`;
+
+
+}
+
+const buttons = document.querySelectorAll('button');
+
+for(let button of buttons) {
+    button.addEventListener('click',colorize) 
+      
+    
+}
+
+const h1s = document.querySelectorAll('h1');
+for (let h1 of h1s){
+    h1.addEventListener('click',colorize) 
+       
+}
+
+function colorize(){
+    this.style.backgroundColor = makeRandomColor();
+    this.style.color = makeRandomColor();
+
+}
