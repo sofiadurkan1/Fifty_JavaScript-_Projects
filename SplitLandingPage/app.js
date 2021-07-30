8556eaa4ba48d10570d3b82867f59eea8861b477
sofia durkan <sofiadurkan@gmail.com>
2021-07-30 10:30:40 -0500

    first commit

---
+++ SplitLandingPage/app.js
@@ -0,0 +1,9 @@
+const content1 = document.querySelector(".content1");
+const content2 = document.querySelector(".content2");
+const container = document.querySelector(".container");
+
+
+content1.addEventListener("mouseover",() =>{
+   
+
+})
