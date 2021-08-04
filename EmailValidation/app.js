cd8f593539e6163781c42198d0dde4f256c8f1ac
sofia durkan <sofiadurkan@gmail.com>
2021-08-03 21:23:37 -0500

    completed

--- EmailValidation/app.js
+++ EmailValidation/app.js
@@ -14,6 +14,8 @@ submitBtn.addEventListener('click', function() {
     }
 })
 
+
+//Enter key
 emailField.addEventListener('keyup', function(event) {
     if(event.code === 'Enter'){
         event.preventDefault();
@@ -28,12 +30,15 @@ function isValid(email) {
     
     // Check if there is exactly one "@" and "."  
     //we need to check here first after that extension
-    const emailWithoutAd = email.replaceAll('@', '');
-    const emailWithoutDot = email.replaceAll('.', '');
+    const emailWithoutAd = email.replaceAll('@', '');  //baska bir karakter yazildiginda @isareti bulamazsa  aradaki fark 0 oluyor. bu da @isartei yok demek.. False
+    const emailWithoutDot = email.replaceAll('.', ''); // bizim istedigimiz aradaki farkin kesinlikle 1 olmasi..bu durumda true dondurecek
     const size = email.length;
 
     if (size - emailWithoutAd.length !== 1 || 
-        size - emailWithoutDot.length !== 1
+        size - emailWithoutDot.length !== 1 ||
+        email.includes(" ") 
+
+    
         ){
         return false;
     }
@@ -42,14 +47,18 @@ function isValid(email) {
     // Check if extension has 2 or 3 letters
     usernameAndWebsitenameExtension = email.split("@");//['username','websitename.extension']
     username = usernameAndWebsitenameExtension[0];
-    WebsitenameExtension = usernameAndWebsitenameExtension[1];
-    extension = WebsitenameExtension.split(".")[1]; // ['websitename','extension']
+    WebsitenameExtension = usernameAndWebsitenameExtension[1].split(".");  // ['websitename','extension']
+    websitename = WebsitenameExtension[0];
+    extension = WebsitenameExtension[1];
     
-    if (extension.length < 2 || extension.length > 3){
+    if (extension.length < 2 || extension.length > 3 ||
+        username.length === 0 || username.length>50 ||
+        websitename.length === 0 || websitename.length>50){
         return false;
     } else {
         return true;
     }
+    
 }
 
 
