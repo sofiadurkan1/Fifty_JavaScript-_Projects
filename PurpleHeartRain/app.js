488b94a1ea519a84fb266129203b4b831cc0067d
sofia durkan <sofiadurkan@gmail.com>
2021-08-04 09:02:56 -0500

    completed

--- PurpleHeartRain/app.js
+++ PurpleHeartRain/app.js
@@ -1,7 +1,41 @@
 function createHeart(){
     const heart = document.createElement("div");
-    heart.classList.add("heart-style");
+    heart.classList.add("heart");
 
-    document.body.appendChild("heart");
+    heart.style.left = Math.random()*100 + "vw"
+    heart.style.animationDuration = Math.random()*2 +3 +"s";
+
+    heart.innerText = "💜";
+
+    document.body.append(heart);
+
+
+setTimeout(() => {
+    heart.remove()
+},5000)
 }
 
+
+setInterval((createHeart),1000);
+
+
+
+function createCodersClan(){
+    const codersClan = document.createElement("div");
+    codersClan.classList.add("coders");
+
+    codersClan.style.left = Math.random()*100 + "vw"
+    codersClan.style.animationDuration = Math.random()*2 +3 +"s";
+
+    codersClan.innerText = "CodersClan";
+
+    document.body.append(codersClan);
+
+
+setTimeout(() => {
+    codersClan.remove()
+},5000)
+
+}
+
+setInterval((createCodersClan),1200);
